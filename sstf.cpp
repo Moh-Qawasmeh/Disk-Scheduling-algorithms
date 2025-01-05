f35ff741fba927762cde00d6d9b1ea0ba1d59a93
Moh-Qawasmeh <116433242+Moh-Qawasmeh@users.noreply.github.com>
2025-01-05 23:39:28 +0300

    .

--- sstf.cpp
+++ sstf.cpp
@@ -63,10 +63,6 @@ using namespace std;
  
 int main() {
     ios_base::sync_with_stdio(0);
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
     cin.tie(0);  cout.tie(0);
     sstf();
     return 0;
